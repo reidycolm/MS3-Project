8a1ed3d4be72474f2c90cced56d5578f70392338
reidycolm <reidycolm@gmail.com>
2021-10-07 01:12:57 +0000

    added delete cuisine functionality

--- app.py
+++ app.py
@@ -201,6 +201,13 @@ def edit_cuisine(cuisine_id):
     return render_template("edit_cuisine.html", cuisine=cuisine)
 
 
+@app.route("/delete_cuisine/<cuisine_id>")
+def delete_cuisine(cuisine_id):
+    mongo.db.cuisines.remove({"_id": ObjectId(cuisine_id)})
+    flash("Cuisine Successfully Deleted")
+    return redirect(url_for("get_cuisines"))
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
