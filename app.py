204662730ffcafcb179f5edfb45d6dc7365e449a
reidycolm <reidycolm@gmail.com>
2021-10-07 03:12:09 +0000

    changed debug to false

--- app.py
+++ app.py
@@ -218,4 +218,4 @@ def delete_cuisine(cuisine_id):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
